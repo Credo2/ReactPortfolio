0f34231f00f7868f8a18ce2434689d179bf8baf2
Credo2 <gil.dacles243@gmail.com>
2025-07-10 01:09:43 +0800

    Finally Figured out Tailwind!!

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -11,15 +11,26 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
-                <div className="bg-amber-150 h-full p-5">
-                    <h1 className="p-3 font-sans text-4xl dark:bg-primary">Primary</h1>
-                    <h1 className="primary-foreground p-3 font-sans text-4xl">foreground</h1>
-                    <h1 className="screen p-3 font-sans text-4xl">screen</h1>
-                    <h1 className="foreground p-3 font-sans text-4xl">foreground</h1>
+
+            {/*     //// Color Pallete
+                --color-baks: hsl(0, 47%, 54%);
+                --color-background: var(--background);
+                --color-bgprime: var(--bg-primary);
+                --color-foreground: var(--foreground-primary);
+                --color-midground: var(--midground-primary);
+                --color-background: var(--background-primary);
+                --color-lights: var(--lights);
+             */}
+
+            <div className="flex min-h-screen flex-col bg-baks">
+                <div className="bg-background p-5">
+                    <h1 className="bg-bgprime p-3 font-sans text-4xl">Primary</h1>
+                    <h1 className="bg-foreground p-3 font-sans text-4xl">foreground</h1>
+                    <h1 className="bg-white p-3 font-serif text-4xl">screen</h1>
+                    <h1 className="bg-foreground p-3 font-sans text-4xl">foreground</h1>
                     <h1 className="bg-midground p-3 font-sans text-4xl">midground</h1>
                     <h1 className="bg-background p-3 font-sans text-4xl">Background</h1>
-                    <h1 className="stars p-3 font-sans text-4xl">Stars</h1>
+                    <h1 className="bg-lights p-3 font-sans text-4xl">Stars</h1>
                     <Tablesprite />
                 </div>
             </div>
