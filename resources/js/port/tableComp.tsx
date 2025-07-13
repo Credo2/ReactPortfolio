f6e6ca586871c40137c7cc5532c56863e131cc85
Credo2 <gil.dacles243@gmail.com>
2025-07-13 21:17:20 +0800

    Table Color Done

--- resources/js/port/tableComp.tsx
+++ resources/js/port/tableComp.tsx
@@ -1,11 +1,13 @@
 function Tablesprite() {
     return (
         <div className="h-tablehit flex flex-col bg-table">
+            <div className="h-[5vh] bg-shadow">Table bottom</div>
+
             {/* Top Shadow */}
             <div className="flex h-full flex-col bg-linear-to-t from-table to-tablelight"></div>
 
-            <Computer />
-            <Keyboard />
+            {/* <Computer />
+            <Keyboard /> */}
             {/* Bottom shadow */}
             <div className="h-[5vh] bg-shadow">Table bottom</div>
         </div>
---
+++ resources/js/port/window.tsx
@@ -0,0 +1,9 @@
+function window() {
+    return (
+        <div className=" bg-amber-500 b">
+            <div className="sdsd"></div>
+        </div>
+    );
+}
+
+export default window;
